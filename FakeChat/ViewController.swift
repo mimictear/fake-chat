57ad30190bb2ceb33428c6a7d0a11c8cdc2d9a4e
vorontsov <mimictear@outlook.com>
2023-10-17 10:52:37 +0200

    Initial Commit

---
+++ FakeChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeChat
+//
+//  Created by ANDREY VORONTSOV on 17.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
